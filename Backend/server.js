c2e7b8bc02add99fe5dffca62449c5b8a0100ec6
Roshan Gawade <roshangawade160@gmail.com>
2025-09-08 22:30:21 +0530

    connected server to database

--- Backend/server.js
+++ Backend/server.js
@@ -1,8 +1,18 @@
-const express = require('express');
-const app = express();
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import connectDb from "./config/db.js";
+const app = express(); 
+
+dotenv.config();
+
+
+
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
+app.use(cors());
+connectDb ();
 
 app.get('/', (req, res) => {
   res.send('Backend is running!');
